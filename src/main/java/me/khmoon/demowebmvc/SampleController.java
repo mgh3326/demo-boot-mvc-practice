a2ab291849f52788ea32d78ada4ba1fd4f5ace91
kwanghyunMoon <mgh3326@naver.com>
2019-07-20 01:42:21 +0900

    update

---
+++ src/main/java/me/khmoon/demowebmvc/SampleController.java
@@ -0,0 +1,16 @@
+package me.khmoon.demowebmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+
+  @RequestMapping("/hello")
+  @ResponseBody
+  public String hello() {
+    return "hello";
+  }
+}
